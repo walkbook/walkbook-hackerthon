663a7b6b17dcfe2bc1b7ae427014bd6eef58317d
Nayoung <ahenaka9@gmail.com>
2021-07-06 21:42:00 +0900

    startapp walkMaps, accounts and add walkMaps templates

--- walkbook/settings.py
+++ walkbook/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sass_processor',
+    'walkMaps',
 ]
 
 MIDDLEWARE = [
--- walkbook/urls.py
+++ walkbook/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import walkMaps.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', walkMaps.views.index, name='index'),
 ]
