fa737e898f534473ad89f54b57fdbdd97ab2e6f8
김세원 <52378625+sewonK@users.noreply.github.com>
2021-07-15 00:27:19 +0900

    Revert "Disable password input when social user logged & Block myinfo url"

--- accounts/static/accounts/js/myinfo.js
+++ accounts/static/accounts/js/myinfo.js
@@ -1,38 +1,30 @@
-import { signupFeature } from "./signupFeature.js";
+import { signupFeature } from './signupFeature.js';
 
 (() => {
-  signupFeature.selectOption("sex", sex);
-  signupFeature.selectOption("age", age);
-  if (is_social) {
-    const passwordInput = document.querySelectorAll("input[type=password]");
-    passwordInput[0].setAttribute("disabled", "True");
-    passwordInput[1].setAttribute("disabled", "True");
-  }
+    signupFeature.selectOption('sex', sex);
+    signupFeature.selectOption('age', age);
 })();
 
 (() => {
-  const passwordInput = document.querySelectorAll("input[type=password]");
-  passwordInput[0].onkeyup = () => signupFeature.handleValidatePw();
-  passwordInput[1].onkeyup = () => signupFeature.handleSamePw();
+	const passwordInput = document.querySelectorAll('input[type=password]');
+	passwordInput[0].onkeyup = () => signupFeature.handleValidatePw();
+	passwordInput[1].onkeyup = () => signupFeature.handleSamePw();
 })();
 
 (() => {
-  const searchLocation = document.getElementById("searchLocation");
-  searchLocation.onclick = () => signupFeature.execLocation();
+	const searchLocation = document.getElementById('searchLocation');
+	searchLocation.onclick = () => signupFeature.execLocation();
 })();
 
 (() => {
-  const myinfoForm = document.getElementById("myinfo-form");
-  myinfoForm.onsubmit = (e) => {
-    e.preventDefault();
-    if (is_social) {
-      e.target.submit();
-    } else {
-      if (signupFeature.validatePassword(signupFeature.getPasswords())) {
-        e.target.submit();
-      } else {
-        signupFeature.dismissSignup("비밀번호");
-      }
+	const myinfoForm = document.getElementById('myinfo-form');
+	myinfoForm.onsubmit = (e) => {
+        e.preventDefault();
+        if (signupFeature.validatePassword(signupFeature.getPasswords())){
+            e.target.submit();
+        }
+        else {
+            signupFeature.dismissSignup('비밀번호');
+        }
     }
-  };
 })();
