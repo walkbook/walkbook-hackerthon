00b6c7eca820861d6ae16d0596325211e144da60
lerry <lerry597@gmail.com>
2021-07-19 20:31:38 +0900

    [feat] edit components.js

--- maps/static/maps/js/components.js
+++ maps/static/maps/js/components.js
@@ -1,4 +1,4 @@
-const geoSuccessMsg = '<div style="width:180px;text-align:center;padding:6px 0;">여기 계시는군요! :D</div>';
+const geoSuccessMsg = '<div class="geoSuccessMsg-class" style="width:180px;text-align:center;padding:6px 0;">여기 계시는군요! :D</div>';
 const geoFailMsg = '현재 위치를 받아올 수 없습니다 :(';
 
 function infoWindowContent(id, title, description) {
