38c03a97f0677358cc03b7f00e11eaed55698e17
sewonK <cutecat1128@naver.com>
2021-07-22 22:00:30 +0900

    Change mypage js to be able to use map(not module)

--- mypage/static/mypage/js/mypage.js
+++ mypage/static/mypage/js/mypage.js
@@ -1,16 +1,70 @@
-import { feature } from './feature.js';
+const MyPosts = {
+    selected : 'register',
+    walkroads : my_walkroads,
+    walkroad_paths : my_walkroad_paths,
 
-(() => {
-    const registeredPosts = document.getElementById('registered-posts');
-    registeredPosts.onclick = () => feature.onClickRegisteredPosts();
-})();
+    onClickRegisteredPosts() {
+        const registeredPosts = this.GetAllRegisteredPosts();
+        const likedPosts = this.GetAllLikedPosts();
+        this.ShowPosts(registeredPosts);
+        this.HidePosts(likedPosts);
+        this.selected = 'register';
+        removeOverlays();
+        draw()
+    },
+    
+    onClickLikedPosts() {
+        const registeredPosts = this.GetAllRegisteredPosts();
+        const likedPosts = this.GetAllLikedPosts();
+        this.ShowPosts(likedPosts);
+        this.HidePosts(registeredPosts);
+        this.selected = 'like';
+        removeOverlays();
+        draw()
+    },
 
-(() => {
-    const likedPosts = document.getElementById('liked-posts');
-    likedPosts.onclick = () => feature.onClickLikedPosts();
-})();
+    GetAllRegisteredPosts() {
+        const RegisteredPosts = Array.from(document.querySelectorAll('.registered-post'));
+        return RegisteredPosts.map((post) => ({
+			post,
+			postTitle: post.textContent.trim(),
+		}));
+    },
 
-(() => {
-    const searchPostBtn = document.getElementById('search-posts-btn');
-    searchPostBtn.onclick = () => feature.onClickSearchBtn();
-})();
+    GetAllLikedPosts() {
+        const LikedPosts = Array.from(document.querySelectorAll('.liked-post'));
+        return LikedPosts.map((post) => ({
+			post,
+			postTitle: post.textContent.trim(),
+		}));
+    },
+
+    ShowPosts(posts) {
+        return posts.forEach(({post}) => post.classList.remove('hide'));
+    },
+
+    HidePosts(posts) {
+        return posts.forEach(({post}) => post.classList.add('hide'));
+    },
+
+    getMatchedPosts(posts, keyword){
+        return posts.filter(({postTitle}) => postTitle.includes(keyword));
+    },
+
+    getUnMatchedPosts(posts, keyword){
+        return posts.filter(({postTitle}) => !postTitle.includes(keyword));
+    },
+
+    onClickSearchBtn(){
+        const keyword = document.getElementById('search-posts').value;
+        let posts = this.GetAllRegisteredPosts()
+        if(this.selected == 'like'){
+            posts = this.GetAllLikedPosts()
+        }
+
+        const MatchedPosts = this.getMatchedPosts(posts, keyword);
+        const unMatchedPosts = this.getUnMatchedPosts(posts, keyword);
+        this.ShowPosts(MatchedPosts);
+        this.HidePosts(unMatchedPosts);
+    }
+};
